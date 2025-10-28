2781f6e732e0757eb6843c31c302f902cea33d3b
Teraz6 <retenk@gmail.com>
2025-10-28 11:46:26 +0200

    INIT

---
+++ TestingTARge24/TestingTARge24/NUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace TestingTARge24
+{
+    public class NUnitTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            driver.Url = "https://www.demoqa.com";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
